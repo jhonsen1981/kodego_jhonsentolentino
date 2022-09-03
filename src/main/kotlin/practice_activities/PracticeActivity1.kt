3416e9e618a653dfefc2c405279516614564bd73
jhonsen <jhonsen.tolentino81@gmail.com>
2022-09-03 16:53:49 +0800

    intial commit for PracticeActivity1.kt

---
+++ src/main/kotlin/practice_activities/PracticeActivity1.kt
@@ -0,0 +1,17 @@
+package practice_activities
+
+fun main(){
+    println("Enter your name")
+    var name : String = readln().toString()
+
+    println("Enter your middle name")
+    var middlename : String = readln().toString()
+
+    println("Enter your last name")
+    var lastname : String = readln().toString()
+
+    println("Enter your name is is $name $middlename $lastname")
+
+
+
+}
