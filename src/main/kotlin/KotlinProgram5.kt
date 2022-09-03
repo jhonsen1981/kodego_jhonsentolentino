f1bceb5d5e4d9b0ffc88c4e47031ef1be10ec659
jhonsen <jhonsen.tolentino81@gmail.com>
2022-09-03 16:31:16 +0800

    intial commit for KotlinProgram5.kt

---
+++ src/main/kotlin/KotlinProgram5.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var itemNumber:Int = 2
+
+    when(itemNumber){
+        1 -> print("clothes")
+        2 -> print("food")
+        3 -> print("toiletries")
+        else -> print("invsalid item number")
+
+    }
+}
