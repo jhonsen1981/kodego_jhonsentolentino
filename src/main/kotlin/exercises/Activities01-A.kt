b275268fd34bf91c649ff7e6e380d3a5293fd8d7
jhonsen <jhonsen.tolentino81@gmail.com>
2022-09-03 13:27:48 +0800

    intial commit for Activities01-A.kt

---
+++ src/main/kotlin/exercises/Activities01-A.kt
@@ -0,0 +1,39 @@
+package exercises
+
+fun main(){
+
+    println("Hello from Jhonsen" )
+
+    //students data
+    //student 1
+    println("Enter student name:")
+    var name1 : String = readln() .toString()
+
+    println("Enter Course:")
+    var course1 : String = readln().toString()
+
+    println("Enter student grade")
+    var grade1 : Int = readln().toInt()
+
+    //student 2
+    println("Enter student name:")
+    var name2 : String = readln() .toString()
+
+    println("Enter Course:")
+    var course2 : String = readln().toString()
+
+    println("Enter student grade")
+    var grade2 : Int = readln().toInt()
+
+    //student 3
+    println("Enter student name:")
+    var name3 : String = readln() .toString()
+
+    println("Enter Course:")
+    var course3 : String = readln().toString()
+
+    println("Enter student grade")
+    var grade3 : Int = readln().toInt()
+
+
+}
---
+++ src/main/kotlin/exercises/Activities01-B.kt
@@ -0,0 +1,24 @@
+package exercises
+fun main(){
+    println("title of the Books")
+
+    //title of the books starts with A
+    var string1 : String = "Anatomy"
+    var string2 : String = "Automotive"
+    var string3 : String = "Animal Farm"
+
+    //title of the books starts with B
+    var string4 : String = "Beach House"
+
+    //string character
+    println(string1[0])
+    println("$string1")
+    println("$string2")
+    println("$string3")
+
+    println(string4[0])
+    println("$string4")
+
+
+
+}
