4effec60f6c65cfc685a9f7156ca6e15de706279
jhonsen <jhonsen.tolentino81@gmail.com>
2022-09-03 13:48:07 +0800

    intial commit for Activities01-C.kt

--- src/main/kotlin/exercises/Activities01-C.kt
+++ src/main/kotlin/exercises/Activities01-C.kt
@@ -1,5 +1,27 @@
 package exercises
 
 fun main(){
+    println("canned goods from grocery")
 
+    //list of canned goods from grocery
+    var string1 : String = "Corned beef"
+    var string2 : String = "Sardines"
+    var string3 : String = "Spam"
+
+    println("chemical products from grocery")
+
+    //List of chemical products from grocery
+    var string4 : String = "Detergent"
+    var string5 : String = "Insect Spray"
+    var string6 : String = "Dishwashing Liquid"
+
+
+    //string character
+    println(string1[0])
+    println("$string1")
+    println("$string2")
+    println("$string3")
+
+    println(string4[0])
+    println("$string4")
 }
