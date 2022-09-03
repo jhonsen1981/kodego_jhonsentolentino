0aa56065972dc4397f0079bc1421b9fadf649fe0
jhonsen <jhonsen.tolentino81@gmail.com>
2022-09-03 09:17:18 +0800

    commit sum program

--- src/main/kotlin/FirstKotlinProgram.kt
+++ src/main/kotlin/FirstKotlinProgram.kt
@@ -1,17 +1,24 @@
 fun main(){
     /*
-    Author: Paulo
+    Author: Jhonsen
     Date Created: 9/1/2022
     Filename: FirstKotlinProgram.kt
 
      */
-    print("Hello World!")
-    print("Hello world!!!!")
-    print("Hello World!!!!!!!!!")
-    print("Hello World!!!!!!!!!!!!!!!!!!")
-    print("Hello World4")
-    print("Hello World5")
-    print("Hello World6")
+    println("Enter number 1:")
+    var number1 : Int = readln() .toInt()
+
+    println("Enter number 2:")
+    var number2 : Int = readln() .toInt()
+
+    var sum : Int = number1 + number2
+    println("The sum is $sum")
+
+
+
+
+
 }
+
 
 
