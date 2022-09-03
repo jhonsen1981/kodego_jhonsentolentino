50049f613d04e636ce5c19107cf0b33a9b5b597a
jhonsen <jhonsen.tolentino81@gmail.com>
2022-09-03 16:33:33 +0800

    intial commit for KotlinProgram4.kt

--- src/main/kotlin/KotlinProgram4.kt
+++ src/main/kotlin/KotlinProgram4.kt
@@ -1,14 +1,27 @@
 fun main(){
-    var number : Int = 10
+    var grade : Int = 74
 
-    if(number < 10 ) {
-        println("Hello")
+    if((grade >=96)&&(grade <=100)){
+        println("1.00")
+    }else if((grade >=86)&&(grade <=95)){
+        println("1.75")
+    }else if((grade >=76)&&(grade <=85)){
+        println("2,25")
+    }else if(grade > 75 ){
+        println("5.0")
+    }else{
+        println("invalid grade!")
 
     }
+    var gender : String = "X"
+    if(gender == "M"){
+     println("Male")
+    }else if(gender =="F"){
+    println("Female")
+    }else{
+        println("Invalid choice")
 
-    var grade : Int =75
-    if(grade >=75 ) {
-        println("you passed!")
+
 
     }
 
