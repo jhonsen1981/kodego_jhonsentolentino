e81d447420f99767539bf96c59082ba4a5ad6b0f
jhonsen <jhonsen.tolentino81@gmail.com>
2022-09-07 22:00:28 +0800

    initial commit for Activities01-D.kt

--- src/main/kotlin/exercises/Activities01-D.kt
+++ src/main/kotlin/exercises/Activities01-D.kt
@@ -1,16 +1,16 @@
-package exercises
+package excercises
 
 fun main(){
     var sum: Int = 0
     var input: String
 
-    do {
-        println("Enter Number:")
+    do{
+        print("Enter Number:")
         input = readLine()!!
         sum += input.toInt()
+
 
     }while (input != "0")
 
     println("sum = $sum")
-    
 }
