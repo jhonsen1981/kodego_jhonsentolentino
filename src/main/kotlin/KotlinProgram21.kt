709e0f20827aaf32e76cd3558158a315611d84d7
jhonsen <jhonsen.tolentino81@gmail.com>
2022-09-10 11:27:57 +0800

    initial commit for KotlinProgram21.kt

---
+++ src/main/kotlin/KotlinProgram21.kt
@@ -0,0 +1,30 @@
+fun main(){
+    //instantiation - process of creating an object
+    var myObject = MyClass("Hello World")
+
+    var myObject2 = MyClass("Hi World")
+}
+
+
+class MyClass(value : String){
+    init{
+        println(value)
+    }
+}
+
+
+
+/*
+Class -> objects
+object -> variable
+       -> function(process)
+uppercase -> first letter
+examples.
+Strng
+Int
+
+
+
+
+
+ */
